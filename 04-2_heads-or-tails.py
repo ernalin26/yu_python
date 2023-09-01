9728332f039611e1928b494943e50948f57ceafe
ernalin26 <ernalin26@gmail.com>
2023-09-01 20:14:56 +1000

    Add files via upload

---
+++ 04-2_heads-or-tails.py
@@ -0,0 +1,11 @@
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+import random
+
+# 🚨 Don't change the code below 👇
+randomNum = random.randint(0, 1)
+
+if randomNum == 0:
+    print("Tails")
+elif randomNum == 1:
+    print("Heads") 
